5f923304f43638b127c04b95a490538efc9e8158
Reka <reka.kovacs1993@gmail.com>
2023-07-08 18:03:58 +0100

    first commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Movement : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Rigidbody2D lightBoy;
+
+    public float speed = 10.5f;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime /2;
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime /2;
+            pos.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+            pos.y -= speed * Time.deltaTime /2;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+            pos.y += speed * Time.deltaTime /2;
+        }
+        
+        lightBoy.transform.position = pos;
+    }
+}
