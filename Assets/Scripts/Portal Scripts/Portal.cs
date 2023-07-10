c14f993249e8a17a5b1f1fc776f292f9513013bc
Daniel FCS <danielf.cs@outlook.com>
2023-07-10 16:19:40 +0100

    portal triggers winner winner message

--- Assets/Scripts/Portal Scripts/Portal.cs
+++ Assets/Scripts/Portal Scripts/Portal.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Portal : MonoBehaviour
 {
 
     public Collider2D collider2D;
 
+    public bool portalOpen = false;
+
+    public GameObject text;
+
+    private void Start()
+    {
+        text.SetActive(false);
+        Debug.Log("You should not see mee teehee");
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.CompareTag("Player"))
         {
-            Debug.Log("I aM TrIGgeRed");
+            portalOpen = true;
+            text.SetActive(portalOpen);
+            Debug.Log("I aM TrIGgeRed by " + collision.name + " and I am now " + portalOpen);
         }
     }
 
