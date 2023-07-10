d7400a9be0850903ad6d901e073358d11f43348d
Alex <alexgjohn@outlook.com>
2023-07-10 13:13:20 +0100

    started working on collision effect with portal

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class Movement : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
     public Tilemap tilemap;
     public Rigidbody2D lightBoy;
