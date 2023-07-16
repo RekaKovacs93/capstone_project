4106641377a43384a9914ee00b05f3089f11275e
Alex <alexgjohn@outlook.com>
2023-07-16 10:27:00 +0100

    added more text to sync up with intro narration

--- Assets/Scripts/IntroTextManager.cs
+++ Assets/Scripts/IntroTextManager.cs
@@ -17,7 +17,16 @@ public class IntroTextManager : MonoBehaviour
 
     [SerializeField]
     private GameObject narrationText3;
-   
+
+    [SerializeField]
+    private GameObject narrationText4;
+
+    [SerializeField]
+    private GameObject narrationText5;
+
+    [SerializeField]
+    private GameObject narrationText6;
+
 
 
     void Start()
@@ -26,6 +35,9 @@ public class IntroTextManager : MonoBehaviour
         narrationText1.SetActive(false);
         narrationText2.SetActive(false);
         narrationText3.SetActive(false);
+        narrationText4.SetActive(false);
+        narrationText5.SetActive(false);
+        narrationText6.SetActive(false);
     }
 
  
@@ -41,7 +53,7 @@ public class IntroTextManager : MonoBehaviour
             narrationText1.SetActive(true);
         }
 
-        if (timer > 20.3)
+        if (timer > 20)
         {
             narrationText2.SetActive(true);
         }
@@ -49,13 +61,26 @@ public class IntroTextManager : MonoBehaviour
         {
             narrationText3.SetActive(true);
         }
-        if (timer > 32)
+        if (timer > 30)
         {
             narrationText1.SetActive(false);
             narrationText2.SetActive(false);
             narrationText3.SetActive(false);
         }
-        if (timer > 40)
+        if (timer > 31)
+        {
+            narrationText4.SetActive(true);
+        }
+        if (timer > 32)
+        {
+            narrationText5.SetActive(true);
+        }
+        if (timer > 33.5)
+        {
+            narrationText4.SetActive(false);
+            narrationText5.SetActive(false);
+        }
+        if (timer > 50)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
