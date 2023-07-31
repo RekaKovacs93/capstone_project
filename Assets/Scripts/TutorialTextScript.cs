941cf7ad79933d07d24fdd1afe60cfe89c4f044b
Alex <alexgjohn@outlook.com>
2023-07-31 11:27:15 +0100

    text triggers only show once now

--- Assets/Scripts/TutorialTextScript.cs
+++ Assets/Scripts/TutorialTextScript.cs
@@ -10,18 +10,21 @@ public class TutorialTextScript : MonoBehaviour
     [SerializeField]
     private GameObject text;
 
-    [SerializeField] Portal portal;
+    //[SerializeField] Portal portal;
 
     //[SerializeField] PortalTrigger crystal;
 
     [SerializeField]
     private Collider2D textTrigger;
 
+    private bool textAppeared;
+
 
     // Start is called before the first frame update
     void Start()
     {
         text.SetActive(false);
+        textAppeared = false;
     }
 
     // Update is called once per frame
@@ -32,21 +35,24 @@ public class TutorialTextScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && textTrigger.CompareTag("Trigger2") && portal.getPortalStatus())
+        if (collision.CompareTag("Player") && !textAppeared)
         {
             text.SetActive(true);
+            
         }
-        else if (collision.CompareTag("Player") && !textTrigger.CompareTag("Trigger2"))
-        {
-            text.SetActive(true);
+        //else if (collision.CompareTag("Player") && !textTrigger.CompareTag("Trigger2") && !textAppeared)
+        //{
+        //    text.SetActive(true);
+            
 
-        }
+        //}
     }
 
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         text.SetActive(false);
+        textAppeared = true;
     }
 
   
